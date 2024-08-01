cef3996fcd430c58215803f9c10b412508c5145e
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-08-01 14:32:31 -0300

    classeauto | getbyid and post

--- classeauto/src/index.ts
+++ classeauto/src/index.ts
@@ -1,6 +1,6 @@
 async function get() {
     try {
-        const response = await fetch('https://apigenerator.dronahq.com/api/RJd-Vfps/gerenciadorcarros');
+        const response = await fetch(`https://apigenerator.dronahq.com/api/RJd-Vfps/gerenciadorcarros`);
         if (!response.ok) {
             throw new Error('Erro de conexão / was not ok');
         }
@@ -12,3 +12,31 @@ async function get() {
     }
 }
 
+async function GetById(id: number) {
+    try {
+        const response = await fetch(`https://apigenerator.dronahq.com/api/RJd-Vfps/gerenciadorcarros${id}`);
+        if (!response.ok) {
+            throw new Error('Erro de conexão / was not ok');
+        }
+        const data = await response.json();
+        console.log(data)
+        return data;
+    }   catch (error) {
+        console.error('Erro de fetching data:', error)
+    }
+}
+
+async function postData(url: string, data: any) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error('Erro de conexão / was not ok')
+        }
+    }
+}
