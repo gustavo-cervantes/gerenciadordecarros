104f4e110176b1c1c5dd563be164b45209e95ef9
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-08-01 14:47:15 -0300

    classeauto | update, delete

--- classeauto/src/index.ts
+++ classeauto/src/index.ts
@@ -1,3 +1,5 @@
+import { json } from "stream/consumers";
+
 async function get() {
     try {
         const response = await fetch(`https://apigenerator.dronahq.com/api/RJd-Vfps/gerenciadorcarros`);
@@ -38,5 +40,44 @@ async function postData(url: string, data: any) {
         if (!response.ok) {
             throw new Error('Erro de conexão / was not ok')
         }
+        const result = await response.json();
+        console.log(result);
+        return result;
+    }   catch (error) {
+        console.error('Erro de posting data:', error);
+    }
+}
+
+async function updateRecord(id: number, updatedData: any) {
+    try {
+        const response = await fetch(`https://apigenerator.dronahq.com/api/RJd-Vfps/gerenciadorcarros${id}`,{
+
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedData)
+        });
+        if (!response.ok) {
+            throw new Error('Erro de conexão / was not ok')
+        }
+        const result = await response.json();
+        console.log(result);
+    }   catch (error) {
+        console.error('Erro de updating data:', error);
+    }
+}
+
+async function deleteRecord(id: number) {
+    try {
+        const response = await fetch(`https://apigenerator.dronahq.com/api/RJd-Vfps/gerenciadorcarros${id}`, {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+            throw new Error('Erro de conexão / was not ok');
+        }
+        console.log('Registro/record deleted');
+    }   catch (error) {
+        console.error('Erro de deleting data:', error);
     }
 }
